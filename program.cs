fb87680fc0c98b96681ab36ef4438df626e8f6a4
foco21 <96326691+foco21@users.noreply.github.com>
2023-03-25 00:05:02 -0700

    Update program.cs
    Adds start up quote to terminal (its the minecraft ending quote )

--- program.cs
+++ program.cs
@@ -24,6 +24,11 @@ public class Program
 
         _client.MessageReceived += MessageReceived;
 
+        
+         // Print a quote to the console
+     Console.WriteLine("Twenty years from now you will be more disappointed by the things that you didn't do than by the ones you did do, So throw off the bowlines, Sail away from the safe harbor, Catch the trade winds in your sails. Explore, dream discover, — Unknown");
+
+        
         await Task.Delay(-1);
     }
 
