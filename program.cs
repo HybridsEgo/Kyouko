b2ec78be0c419567159697bb040214d01c614b2b
foco21 <96326691+foco21@users.noreply.github.com>
2023-03-26 00:32:59 -0700

    Update program.cs
    updated main to have colored text and exit program small changes but still untested

--- program.cs
+++ program.cs
@@ -28,12 +28,17 @@ public class Program
         await _client.StartAsync();
 
         _client.MessageReceived += MessageReceived;
-     
-     Console.WriteLine("Twenty years from now you will be more disappointed by the things that you didn't do than by the ones you did do, So throw off the bowlines, Sail away from the safe harbor, Catch the trade winds in your sails. Explore, dream discover, — Unknown");
-        
-        await Task.Delay(-1);
-    }
 
+        Console.ForegroundColor = ConsoleColor.Green;
+        // Print a quote to the console
+        Console.WriteLine("Twenty years from now you will be more disappointed by the things that you didn't do than by the ones you did do, So throw off the bowlines, Sail away from the safe harbor, Catch the trade winds in your sails. Explore, dream discover, — Unknown");
+
+        // Wait for the user to press a key before exiting
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+
+        await _client.StopAsync();
+    }
     private async Task MessageReceived(SocketMessage message)
     {
         string Filename = "trainingdata.json";
@@ -52,7 +57,7 @@ public class Program
 
         Dictionary<string, string> blacklist = new Dictionary<string, string>()
         {
-            {"", "filtered"},
+            {"insert slur here", "filtered"},
 
         };
 
@@ -113,7 +118,7 @@ public class Program
             if (response.Contains(key.ToLower()))
             {
                 Console.WriteLine(blacklist[key]);
-                
+
                 await message.Channel.SendMessageAsync("Filtered");
                 return;
             }
@@ -167,3 +172,4 @@ public class Program
         return Task.CompletedTask;
     }
 }
+
